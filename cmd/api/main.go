67e5c5a82e915de14218ba0f50f6b9cc28969b0e
NakamaShp <khlfaalhsn5@gmail.com>
2025-12-07 13:04:31 +0700

    chore(config): refactor main wiring and configuration settings

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,16 +4,15 @@ import (
 	"context"
 	"log"
 
-	// Import package internal
 	"backend-go/internal/handler"
 	"backend-go/internal/repository"
-	"backend-go/internal/router" // <-- IMPORT ROUTER YANG BARU
+	"backend-go/internal/router" 
 	"backend-go/internal/service"
 	"backend-go/pkg/database"
 )
 
 func main() {
-	// 1. Setup Context & Database
+	
 	ctx := context.Background()
 	firestoreClient, authClient, err := database.InitFirebase(ctx)
 	if err != nil {
@@ -22,24 +21,21 @@ func main() {
 	defer firestoreClient.Close()
 	log.Println("✅ Sukses terhubung ke Firebase!")
 
-	// 2. Wiring (Dependency Injection)
 	
-	// A. Event Module
 	eventRepo := repository.NewFirebaseRepo(firestoreClient)
 	eventService := service.NewEventService(eventRepo)
 	eventHandler := handler.NewEventHandler(eventService)
 
-	// B. User Module
+
 	userRepo := repository.NewUserRepository(firestoreClient)
 	userService := service.NewUserService(userRepo)
 	userHandler := handler.NewUserHandler(userService)
 
-	// C. Middleware
 	authMiddleware := handler.AuthMiddleware(authClient)
 
 	r := router.SetupRouter(eventHandler, userHandler, authMiddleware, userService)
 
-	// 4. Jalankan Server
+	
 	log.Println("🚀 Server berjalan di http://localhost:8081")
 	if err := r.Run(":8081"); err != nil {
 		log.Fatalf("Gagal menjalankan server: %v", err)
--- config/config.go
+++ config/config.go
@@ -1,4 +1,4 @@
 package config
 
-// Nanti kita isi konfigurasi disini
+
 type Config struct {}
